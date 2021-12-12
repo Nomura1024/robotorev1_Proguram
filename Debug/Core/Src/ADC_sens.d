08ebbb68b2a7aeaae12b5dbd7bb2f660cd3449e9
Nomura1024 <n.hayato427@gmail.com>
2021-12-12 20:29:28 +0900

    for_commit

--- Debug/Core/Src/ADC_sens.d
+++ Debug/Core/Src/ADC_sens.d
@@ -1,5 +1,5 @@
-Core/Src/Flash_F405.o: ../Core/Src/Flash_F405.c ../Core/Inc/Flash_F405.h \
- ../Core/Inc/main.h ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+Core/Src/ADC_sens.o: ../Core/Src/ADC_sens.c ../Core/Inc/main.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
  ../Core/Inc/stm32f4xx_hal_conf.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
@@ -31,9 +31,9 @@ Core/Src/Flash_F405.o: ../Core/Src/Flash_F405.c ../Core/Inc/Flash_F405.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
-
-../Core/Inc/Flash_F405.h:
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ ../Core/Inc/initial.h ../Core/Inc/main.h ../Core/Inc/ADC_sens.h \
+ ../Core/Inc/AQM0802.h ../Core/Inc/flash.h
 
 ../Core/Inc/main.h:
 
@@ -102,3 +102,13 @@ Core/Src/Flash_F405.o: ../Core/Src/Flash_F405.c ../Core/Inc/Flash_F405.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
 
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+
+../Core/Inc/initial.h:
+
+../Core/Inc/main.h:
+
+../Core/Inc/ADC_sens.h:
+
+../Core/Inc/AQM0802.h:
+
+../Core/Inc/flash.h:
